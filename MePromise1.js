4804e5570a2d328e84390de7dd9e338b6a7b390c
MokeyJ <997840113@qq.com>
2018-08-22 11:34:17 +0800

    修改了试试分支怎么弄

--- MePromise1.js
+++ MePromise1.js
@@ -1,6 +1,6 @@
 
 //下面我们来写一个简单的 promsie。Promise 的参数是函数 fn，把内部定义 resolve 方法作为参数传到 fn 中，调用 fn。当异步操作成功后会调用 resolve 方法，然后就会执行 then 中注册的回调函数。
-
+// 修改了一下试试分支的作用
 function MePromise(fn){
 	//需要一个成功时的回调函数
 	var callback;
